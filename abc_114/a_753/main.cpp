db565512184259eb692629c420e83762b52e13ec
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-03-19 12:21:43 +0000

    add abc_114 a

---
+++ abc_114/a_753/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int age;
+  cin >> age;
+  if ( age == 7 || age == 5 || age == 3 )
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
+  return 0;
+}
