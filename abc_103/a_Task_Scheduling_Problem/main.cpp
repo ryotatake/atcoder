036f813caad5689492d68d3a46d7b8da13612518
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-03-21 07:45:57 +0000

    add abc_103 a

---
+++ abc_103/a_Task_Scheduling_Problem/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  vector<int> v{ a, b, c };
+  sort( v.begin(), v.end() );
+  cout << v[2] - v[0] << endl;
+  return 0;
+}
